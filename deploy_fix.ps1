d6c380bdb28220c6d62e81027f110c7ced957316
Upendra Kumar <upendra@email.com>
2026-01-02 14:04:32 +0530

    Fix PDF link path to root-relative

---
+++ deploy_fix.ps1
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Fix PDF link path to root-relative"
+git push origin main
--- deploy_pdf.ps1
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Add and link Value Selling Discovery Script PDF"
-git push origin main
